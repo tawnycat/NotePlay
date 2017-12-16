0d565bd8a557cd8e35f981f430751222530751bd
tawnycat <tawnyeleanor@gmail.com>
2017-12-16 08:46:02 -0500

    fixed up the css

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ var sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-    sequelize = new Sequelize(database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 var db = {};
  
